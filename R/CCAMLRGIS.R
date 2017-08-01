681279f5d16817911c1e0a8852a7329c454f8ffd
lucymerobinson <lucy.robinson@ccamlr.org>
2017-08-02 08:47:07 +1000

    updated package documentation

--- R/CCAMLRGIS.R
+++ R/CCAMLRGIS.R
@@ -3,11 +3,11 @@
 #' The CCAMLRGIS package provides two categories of functions: load functions and create functions 
 #' 
 #' @section load functions are used to import CCAMLR Reference into R (ideally from the geoserver) and include:
-#' load_ASDs, load_SSRUs, load_RBs, load_SSMUs, load_MAs and load_EEZs provide easy loading of CCAMLR reference data 
+#' load_ASDs, load_SSRUs, load_RBs, load_SSMUs, load_MAs, load_Coastline, load_RefAreas and load_MPAs provide easy loading of CCAMLR Reference data 
 #' Data that is classified as "master" data is available in "GEOJSON" format and "RDATA" format is available in all load functions 
 #' 
 #' @section create functions are used to create spatial data that is compatible with CCAMLR Reference data and the CCAMLR online GIS. The include:
-#'  create_Polys, create_Points, create_Lines, create_PolyGrids
+#'  create_Polys
 #'  Other functions that support the create functions include Clip2Coast and DensifyData
 #'  
 #' @docType package
