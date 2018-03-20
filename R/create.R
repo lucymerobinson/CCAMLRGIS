40390d4fa2dd1a78f5bb91905f1484c1355c90dc
lucymerobinson <lucy.robinson@ccamlr.org>
2018-03-20 16:59:03 +1100

    removed Type param from create_PolyGrids

--- R/create.R
+++ R/create.R
@@ -36,9 +36,9 @@
 create_Polys=function(InputFile,OutputFormat="ROBJECT",OutputName=NULL,Buffer=0,Separate=0,Densify=1,Clip=0){
   # Load data
   if (class(InputFile)=="character"){
-  data=read.csv(InputFile)}else{
-  data=InputFile  
-  }
+    data=read.csv(InputFile)}else{
+      data=InputFile  
+    }
   IDs=as.character(data[,1])
   ListIDs=sort(unique(IDs))
   Lats=data[,2]
@@ -80,7 +80,7 @@ create_Polys=function(InputFile,OutputFormat="ROBJECT",OutputName=NULL,Buffer=0,
       
       #Project Lat/Lon
       PRO=spTransform(Coords,CRS(CRSProj))
- 
+      
       
       #Add Buffer
       if (Buffer>0){
@@ -114,8 +114,8 @@ create_Polys=function(InputFile,OutputFormat="ROBJECT",OutputName=NULL,Buffer=0,
       proj4string(SPDF)=CRS(CRSProj)
       
       if(OutputFormat=="SHAPEFILE"){
-      if (Separate==1 & Buffer==0){writeOGR(SPDF,".",paste(OutputName,"_",as.character(PID),sep=""),driver="ESRI Shapefile")}
-      if (Separate==1 & Buffer!=0){writeOGR(SPDF,".",paste(OutputName,"_Buffered_",as.character(PID),sep=""),driver="ESRI Shapefile")}
+        if (Separate==1 & Buffer==0){writeOGR(SPDF,".",paste(OutputName,"_",as.character(PID),sep=""),driver="ESRI Shapefile")}
+        if (Separate==1 & Buffer!=0){writeOGR(SPDF,".",paste(OutputName,"_Buffered_",as.character(PID),sep=""),driver="ESRI Shapefile")}
       }
       #Add each polygon to the Group
       Group[[i]] = Pls
@@ -145,7 +145,7 @@ create_Polys=function(InputFile,OutputFormat="ROBJECT",OutputName=NULL,Buffer=0,
       
       #Project Lat/Lon
       PRO=spTransform(Coords,CRS(CRSProj))
-
+      
       
       #Add Buffer
       if (Buffer>0){
@@ -175,12 +175,12 @@ create_Polys=function(InputFile,OutputFormat="ROBJECT",OutputName=NULL,Buffer=0,
       if ((dim(data)[2])==4){df=data.frame(name=PID,row.names=PID,PVal,AreaKm2=PArea);colnames(df)[2]=names(data)[4]}
       if ((dim(data)[2])>4){df=data.frame(name=PID,row.names=PID,PVal,AreaKm2=PArea)}
       
-
+      
       SPDF=SpatialPolygonsDataFrame(SPls, df)
       proj4string(SPDF)=CRS(CRSProj)
       if(OutputFormat=="SHAPEFILE"){
-      if (Separate==1 & Buffer==0){writeOGR(SPDF,".",paste(OutputName,"_",as.character(PID),sep=""),driver="ESRI Shapefile")}
-      if (Separate==1 & Buffer!=0){writeOGR(SPDF,".",paste(OutputName,"_Buffered_",as.character(PID),sep=""),driver="ESRI Shapefile")}
+        if (Separate==1 & Buffer==0){writeOGR(SPDF,".",paste(OutputName,"_",as.character(PID),sep=""),driver="ESRI Shapefile")}
+        if (Separate==1 & Buffer!=0){writeOGR(SPDF,".",paste(OutputName,"_Buffered_",as.character(PID),sep=""),driver="ESRI Shapefile")}
       }
       
       #Add each polygon to the Group
@@ -193,10 +193,10 @@ create_Polys=function(InputFile,OutputFormat="ROBJECT",OutputName=NULL,Buffer=0,
   Group=SpatialPolygonsDataFrame(Group,GroupData)
   proj4string(Group)=CRS(CRSProj)
   if(OutputFormat=="SHAPEFILE"){
-  if (Separate==1 & Buffer==0){writeOGR(Group,".",paste(OutputName,"_Group",sep=""),driver="ESRI Shapefile")}
-  if (Separate==1 & Buffer!=0){writeOGR(Group,".",paste(OutputName,"_Buffered_Group",sep=""),driver="ESRI Shapefile")}
-  if (Separate==0 & Buffer==0){writeOGR(Group,".",OutputName,driver="ESRI Shapefile")}
-  if (Separate==0 & Buffer!=0){writeOGR(Group,".",paste(OutputName,"_Buffered",sep=""),driver="ESRI Shapefile")}
+    if (Separate==1 & Buffer==0){writeOGR(Group,".",paste(OutputName,"_Group",sep=""),driver="ESRI Shapefile")}
+    if (Separate==1 & Buffer!=0){writeOGR(Group,".",paste(OutputName,"_Buffered_Group",sep=""),driver="ESRI Shapefile")}
+    if (Separate==0 & Buffer==0){writeOGR(Group,".",OutputName,driver="ESRI Shapefile")}
+    if (Separate==0 & Buffer!=0){writeOGR(Group,".",paste(OutputName,"_Buffered",sep=""),driver="ESRI Shapefile")}
   }else{
     return(Group)
   }  
@@ -222,11 +222,6 @@ create_PolyGrids=function(InputFile,OutputFormat="ROBJECT",OutputName=NULL,dlon,
     data=read.csv(InputFile)}else{
       data=InputFile  
     }
-  
-  # suppressWarnings(SpatialPolygons())
-  # 
-  # suppressMessages("In `[<-`(`*tmp*`, i, value = <S4 object of class Polygons>):implicit list embedding of S4 objects is deprecated")
-  
   lat=data[,1]
   lon=data[,2]
   Val=data[,3]
@@ -239,81 +234,79 @@ create_PolyGrids=function(InputFile,OutputFormat="ROBJECT",OutputName=NULL,dlon,
   Glon=(ceiling((lon+dlon)/dlon)*dlon-dlon)-dlon/2
   Glat=(ceiling((lat+dlat)/dlat)*dlat-dlat)-dlat/2
   
-  # Set Categorical (Type="CAT") or numerical variable (Type="NUM")
-  if (Type=="NUM"){
-    GVal=Val
-    GriddedData=as.data.frame(cbind(Glon,Glat,GVal))
+  GVal=Val
+  GriddedData=as.data.frame(cbind(Glon,Glat,GVal))
+  
+  GriddedData_Mean=aggregate(GVal~Glon+Glat,data=GriddedData,mean)
+  
+  GLONS=GriddedData_Mean[,1]
+  GLATS=GriddedData_Mean[,2]
+  MEANS=GriddedData_Mean[,3]
+  
+  GriddedData_Sum=aggregate(GVal~Glon+Glat,data=GriddedData,sum)
+  SUMS=GriddedData_Sum[,3]
+  GriddedData_Median=aggregate(GVal~Glon+Glat,data=GriddedData,median)
+  MEDIANS=GriddedData_Median[,3]
+  GriddedData_Max=aggregate(GVal~Glon+Glat,data=GriddedData,max)
+  MAXS=GriddedData_Max[,3]
+  GriddedData_Min=aggregate(GVal~Glon+Glat,data=GriddedData,min)
+  MINS=GriddedData_Min[,3]
+  GriddedData_StDev=aggregate(GVal~Glon+Glat,data=GriddedData,sd)
+  STDEVS=GriddedData_StDev[,3]
+  GriddedData_Count=aggregate(rep(1,length(Glon))~Glon+Glat,data=GriddedData,sum)
+  COUNTS=GriddedData_Count[,3]
+  
+  for (i in (1:length(GLONS))){ #Loop over polygons
     
-    GriddedData_Mean=aggregate(GVal~Glon+Glat,data=GriddedData,mean)
+    PLon=c(GLONS[i]-dlon/2,
+           GLONS[i]+dlon/2,
+           GLONS[i]+dlon/2,
+           GLONS[i]-dlon/2)
+    PLat=c(GLATS[i]+dlat/2,
+           GLATS[i]+dlat/2,
+           GLATS[i]-dlat/2,
+           GLATS[i]-dlat/2)
+    Pmean=round(MEANS[i],2)
+    Psum=round(SUMS[i],2)
+    Pmed=round(MEDIANS[i],2)
+    Pmax=round(MAXS[i],2)
+    Pmin=round(MINS[i],2)
+    Pstdev=round(STDEVS[i],2)
+    Pcount=COUNTS[i]
     
-    GLONS=GriddedData_Mean[,1]
-    GLATS=GriddedData_Mean[,2]
-    MEANS=GriddedData_Mean[,3]
+    #Project Lat/Lon
+    PRO=project(cbind(PLon,PLat),CRSProj)
+    PLon=c(PRO[,1],PRO[1,1])
+    PLat=c(PRO[,2],PRO[1,2])
+    rm(PRO)
     
-    GriddedData_Sum=aggregate(GVal~Glon+Glat,data=GriddedData,sum)
-    SUMS=GriddedData_Sum[,3]
-    GriddedData_Median=aggregate(GVal~Glon+Glat,data=GriddedData,median)
-    MEDIANS=GriddedData_Median[,3]
-    GriddedData_Max=aggregate(GVal~Glon+Glat,data=GriddedData,max)
-    MAXS=GriddedData_Max[,3]
-    GriddedData_Min=aggregate(GVal~Glon+Glat,data=GriddedData,min)
-    MINS=GriddedData_Min[,3]
-    GriddedData_StDev=aggregate(GVal~Glon+Glat,data=GriddedData,sd)
-    STDEVS=GriddedData_StDev[,3]
-    GriddedData_Count=aggregate(rep(1,length(Glon))~Glon+Glat,data=GriddedData,sum)
-    COUNTS=GriddedData_Count[,3]
+    #Create individual Polygons
+    Pl=Polygon(cbind(PLon,PLat))
+    Pls=Polygons(list(Pl), ID=i)
+    SPls=SpatialPolygons(list(Pls))
     
-    for (i in (1:length(GLONS))){ #Loop over polygons
-      
-      PLon=c(GLONS[i]-dlon/2,
-             GLONS[i]+dlon/2,
-             GLONS[i]+dlon/2,
-             GLONS[i]-dlon/2)
-      PLat=c(GLATS[i]+dlat/2,
-             GLATS[i]+dlat/2,
-             GLATS[i]-dlat/2,
-             GLATS[i]-dlat/2)
-      Pmean=round(MEANS[i],2)
-      Psum=round(SUMS[i],2)
-      Pmed=round(MEDIANS[i],2)
-      Pmax=round(MAXS[i],2)
-      Pmin=round(MINS[i],2)
-      Pstdev=round(STDEVS[i],2)
-      Pcount=COUNTS[i]
-      
-      #Project Lat/Lon
-      PRO=project(cbind(PLon,PLat),CRSProj)
-      PLon=c(PRO[,1],PRO[1,1])
-      PLat=c(PRO[,2],PRO[1,2])
-      rm(PRO)
-      
-      #Create individual Polygons
-      Pl=Polygon(cbind(PLon,PLat))
-      Pls=Polygons(list(Pl), ID=i)
-      SPls=SpatialPolygons(list(Pls))
-      
-      df=data.frame(name=names(data)[3],row.names=i,Pcount,Pmax,Pmin,Pmean,Pstdev,Pmed,Psum)
-      colnames(df)[2:8]=c("Count","Max","Min","Mean","StdDev","Median","Sum")
-      
-      SPDF=SpatialPolygonsDataFrame(SPls, df)
-      proj4string(SPDF)=CRS(CRSProj)
-      
-      #Add each polygon to the Group
-      Group[[i]]=Pls
-      GroupData=rbind(GroupData,df)
-    }
+    df=data.frame(name=names(data)[3],row.names=i,Pcount,Pmax,Pmin,Pmean,Pstdev,Pmed,Psum)
+    colnames(df)[2:8]=c("Count","Max","Min","Mean","StdDev","Median","Sum")
     
-    #Collate Group
-    Group=SpatialPolygons(Group)
-    Group=SpatialPolygonsDataFrame(Group,GroupData)
-    proj4string(Group)=CRS(CRSProj)
-    if(OutputFormat=="SHAPEFILE"){
-      writeOGR(Group,".",OutputName,driver="ESRI Shapefile")
-    }else{
-      return(Group)
-    }
+    SPDF=SpatialPolygonsDataFrame(SPls, df)
+    proj4string(SPDF)=CRS(CRSProj)
+    
+    #Add each polygon to the Group
+    Group[[i]]=Pls
+    GroupData=rbind(GroupData,df)
+  }
+  
+  #Collate Group
+  Group=SpatialPolygons(Group)
+  Group=SpatialPolygonsDataFrame(Group,GroupData)
+  proj4string(Group)=CRS(CRSProj)
+  if(OutputFormat=="SHAPEFILE"){
+    writeOGR(Group,".",OutputName,driver="ESRI Shapefile")
+  }else{
+    return(Group)
   }
 }
+
 
 #' Create Lines 
 #'
@@ -523,7 +516,7 @@ create_Lines=function(InputFile,OutputFormat="ROBJECT",OutputName=NULL,Buffer=0,
         if ((dim(data)[2])>4){df=data.frame(name=PID,row.names=PID,PVal,LengthKm=Length,LengthNm=Lengthnm)}
         SPDF=SpatialLinesDataFrame(SPls, df)
         proj4string(SPDF)=CRS(CRSProj)
-         #Add each Line to the Group
+        #Add each Line to the Group
         Group[[i]]=Pls
         GroupData=rbind(GroupData,df)
         
